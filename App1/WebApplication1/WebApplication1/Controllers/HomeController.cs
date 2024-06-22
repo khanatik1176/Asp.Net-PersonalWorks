5170260c33ef1ef0b3b6dd7152dd5c704241f68b
khanatik1176 <khanatik1176@gmail.com>
2024-06-22 22:57:46 +0600

    Simple School CRUD system using DTO and EF

---
+++ App1/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.EF;
+using WebApplication1.DTOs;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Registration()
+        {   
+           return View();
+        }
+        [HttpPost]
+        public ActionResult Registration(StudentDTO s)
+        {
+            School_dbEntities2 db = new School_dbEntities2();
+
+            if(ModelState.IsValid)
+            {
+                var st = new Student()
+                {
+                    Name = s.FirstName.Trim()+" "+ s.LastName.Trim(),
+                    Email = s.Email,
+                    Address = s.Address
+
+                };
+
+                db.Students.Add(st);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Error = "Please fill the data properly";
+            }
+
+            return View(s);
+        }
+
+        public ActionResult ViewStudents()
+        {
+            School_dbEntities2 db = new School_dbEntities2();
+
+            var data = db.Students.ToList();
+  
+            return View(data);
+        }
+
+        [HttpGet]
+        public ActionResult EditStudents(int id)
+        {
+            School_dbEntities2 db = new School_dbEntities2();
+            var exobj = db.Students.Find(id);
+            return View(exobj);
+        }
+
+        [HttpPost]
+
+        public ActionResult EditStudents(Student s)
+        {
+            School_dbEntities2 db = new School_dbEntities2();
+            var exobj = db.Students.Find(s.id);
+            exobj.Name = s.Name;
+            exobj.Email = s.Email;
+            exobj.Address = s.Address;
+
+            db.SaveChanges();
+            return RedirectToAction("ViewStudents");
+        }
+
+        [HttpGet]
+        public ActionResult DeleteStudents(Student s)
+        {
+            School_dbEntities2 db = new School_dbEntities2();
+            var exobj = db.Students.Find(s.id);
+
+            if(exobj != null)
+            {
+                db.Students.Remove(exobj);
+                db.SaveChanges();
+            }
+            return RedirectToAction("ViewStudents");
+        }
+
+        public ActionResult Search(string Search)
+        {
+            School_dbEntities2 db = new School_dbEntities2();
+
+            var data = (from s in db.Students
+                        where s.Email.Contains(Search)
+                        select s).ToList();
+
+            return View("ViewStudents", data);
+        }
+
+
+
+    }
+}
---
+++ App1/WebApplication1/WebApplication1/DTOs/StudentDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.DTOs
+{
+    public class StudentDTO
+    {
+        public int id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+    }
+}
